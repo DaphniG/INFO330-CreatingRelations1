f1ec01e5ad4fd253f14145bec61f8517d845e6b6
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-04-13 11:21:35 -0700

    Create students.sql

---
+++ students.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students (id int primary key, firstname varchar(40), lastname varchar(80), age int);
+
+insert into Students(id, firstname, lastname, age) values(1,'Fred','Flintstone',35);
+insert into Students(id, firstname, lastname, age) values(2, 'Wilma', 'Flintstone', 29);
+insert into Students(id, firstname, lastname, age) values(3, 'Barney','Rubble', 33);
+insert into Students(id, firstname, lastname, age) values(4, 'Betty', 'Rubble', 29);
+insert into Students(id, firstname, lastname, age) values(5, 'Pebbles', 'Flintstone', 1);
+insert into Students(id, firstname, lastname, age) values(6, 'Bam-Bam', 'Rubble', 1);
